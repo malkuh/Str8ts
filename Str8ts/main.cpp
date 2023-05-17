797095b949d28de096ad90dca4cad4019b792e02
Hp i <sql.904@me.com>
2023-05-17 11:09:06 +0200

    Initial Commit

---
+++ Str8ts/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Str8ts
+//
+//  Created by Hans-Peter Bodden on 17.05.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
